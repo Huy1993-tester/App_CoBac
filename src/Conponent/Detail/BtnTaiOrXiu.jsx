6d4c0483c40244b225edf425444f63e3712ce11a
Huy1993-tester <maiquochuy999@gmail.com>
2021-04-21 19:16:33 +0700

    update

--- src/Conponent/Detail/BtnTaiOrXiu.jsx
+++ src/Conponent/Detail/BtnTaiOrXiu.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 
 export default class BtnTaiOrXiu extends Component {
+  state = {
+    color: "bg-info display-4 p-5 text-light",
+  };
+  changeColor = () => {
+    this.setState({
+      color: "bg-danger display-4 p-5 text-light",
+    });
+  };
   render() {
     const { taiOrXiu, handleChoice } = this.props;
     return (
       <button
-        className="bg-danger display-4 p-5 text-light"
+        className={this.state.color}
         onClick={() => handleChoice(taiOrXiu)}
       >
         {taiOrXiu ? "Tài" : "Xỉu"}
